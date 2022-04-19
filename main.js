61b410621d0163ae307d75cbaf329891f5fe9880
timo.scheer <timo.scheer@bytediver.de>
2022-04-19 14:27:56 +0200

    added p5

--- main.js
+++ main.js
@@ -9,25 +9,27 @@ function get3randomNumbers() {
     return randomNumbers;
 }
 
-document.getElementById('output').innerHTML = get3randomNumbers();
+document.getElementById('output').innerHTML = solution;
 
 
 function getInput() {
-    let input = document.getElementById("input").value;
-    console.log(input);
+    let input0 = parseInt(document.getElementById("input_0").value);
+    let input1 = parseInt(document.getElementById("input_1").value);
+    let input2 = parseInt(document.getElementById("input_2").value);
+    let input = [input0, input1, input2];
+
+    for (let i = 0; i < solution.length; ++i) {
+        if (input[i] === solution[i]) {
+            document.getElementById("input_container_" + i).style.backgroundColor = "#149414";
+            document.getElementById("input_container_" + i).style.borderColor = "#149414";
+        } else {
+            document.getElementById("input_container_" + i).style.backgroundColor = "red";
+            document.getElementById("input_container_" + i).style.borderColor = "red";
+        }
+    }
+    if (input[0] === solution[0] && input[1] === solution[1] && input[2] === solution[2]) {
+        document.getElementById("output").innerHTML = "You win!";
+    }
+
     return input;
 }
-
-function matchInput() {
-    let input = getInput();
-    let matchingPositions = [];
-    solution.forEach((element, index) => {
-        if (input === element) {
-            matchingPositions.push(index);
-        }
-        console.log(matchingPositions);
-    });
-    return matchingPositions;
-}
-
-console.log(matchInput());
---
+++ sketch.js
@@ -0,0 +1,4 @@
+function setup() {
+    let canvas = createCanvas(300, 300);
+    canvas.parent('sketch-container');
+}
